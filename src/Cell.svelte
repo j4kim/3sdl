aea91cbb356cfc91ca1dcf02948f7b5b3dcc29ad
Joaquim Perez <jivkim@gmail.com>
2023-03-22 19:14:54 +0100

    a new Cell component

---
+++ src/Cell.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let px;
+  export let x;
+  export let y;
+  export let fill;
+</script>
+
+<rect
+  data-x={x}
+  data-y={y}
+  x={(px * x).toFixed(2)}
+  y={(px * y).toFixed(2)}
+  width={px}
+  height={px}
+  {fill}
+/>
--- src/Row.svelte
+++ src/Row.svelte
@@ -1,17 +1,11 @@
 <script>
+  import Cell from "./Cell.svelte";
+
   export let row;
   export let px;
   export let y;
 </script>
 
 {#each row as cell, x}
-  <rect
-    data-x={x}
-    data-y={y}
-    x={px * x}
-    y={px * y}
-    width={px}
-    height={px}
-    fill={cell === "X" ? "white" : ""}
-  />
+  <Cell {x} {y} {px} fill={cell === "X" ? "white" : ""} />
 {/each}
