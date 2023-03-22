df6ad6d6cb0cc014b899922433ce7634ab62fb6d
Joaquim Perez <jivkim@gmail.com>
2023-03-22 18:06:35 +0100

    A new Map component to handle drawing logic

--- src/App.svelte
+++ src/App.svelte
@@ -1,44 +1,18 @@
 <script>
   import map from "./map.js";
-  import Row from "./Row.svelte";
+  import Map from "./Map.svelte";
 
-  let clientWidth = 100;
-  let clientHeight = 100;
-
-  const mapRows = map.length;
-  const mapCols = map[0].length;
-  const defaultRow = " ".repeat(mapCols);
+  let clientWidth;
+  let clientHeight;
 
   $: width = 0.98 * clientWidth;
   $: maxHeight = 0.99 * clientHeight;
-
-  $: px = +(width / mapCols).toFixed(2);
-
-  $: mapHeight = px * mapRows;
-
-  $: remainingHeight = maxHeight - mapHeight;
-
-  $: additionalRows = Math.floor(remainingHeight / px);
-
-  $: height = Math.max(mapHeight + additionalRows * px, mapHeight);
-
-  $: rowsBefore = Math.round(additionalRows / 2);
-
-  $: rowsAfter = additionalRows - rowsBefore;
-
-  $: allRows = [
-    ...(rowsBefore > 0 ? Array(rowsBefore).fill(defaultRow) : []),
-    ...map,
-    ...(rowsAfter > 0 ? Array(rowsAfter).fill(defaultRow) : []),
-  ];
 </script>
 
 <main bind:clientWidth bind:clientHeight>
-  <svg {width} {height} fill="rgb(31, 38, 49)">
-    {#each allRows as row, y}
-      <Row {row} {y} {px} />
-    {/each}
-  </svg>
+  {#if clientHeight}
+    <Map {map} {width} {maxHeight} />
+  {/if}
 </main>
 
 <style>
@@ -52,8 +26,5 @@
     align-items: center;
     justify-content: center;
     overflow: hidden;
-  }
-  svg {
-    display: block;
   }
 </style>
---
+++ src/Map.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Row from "./Row.svelte";
+
+  export let map;
+  export let width;
+  export let maxHeight;
+
+  $: mapRows = map.length;
+  $: mapCols = map[0].length;
+  $: defaultRow = " ".repeat(mapCols);
+
+  $: px = +(width / mapCols).toFixed(2);
+
+  $: mapHeight = px * mapRows;
+
+  $: remainingHeight = maxHeight - mapHeight;
+
+  $: additionalRows = Math.floor(remainingHeight / px);
+
+  $: height = Math.max(mapHeight + additionalRows * px, mapHeight);
+
+  $: rowsBefore = Math.round(additionalRows / 2);
+
+  $: rowsAfter = additionalRows - rowsBefore;
+
+  $: rows = [
+    ...(rowsBefore > 0 ? Array(rowsBefore).fill(defaultRow) : []),
+    ...map,
+    ...(rowsAfter > 0 ? Array(rowsAfter).fill(defaultRow) : []),
+  ];
+</script>
+
+<svg {width} {height} fill="rgb(31, 38, 49)">
+  {#each rows as row, y}
+    <Row {row} {y} {px} />
+  {/each}
+</svg>
+
+<style>
+  svg {
+    display: block;
+  }
+</style>
